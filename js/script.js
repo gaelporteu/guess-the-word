58f3446e9ae138f2de07dbd2ab626a4f8346d4a7
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-07-20 16:20:48 -0400

    Add updateWordInProgress function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const lettersGuessed = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const guessLetter = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -12,13 +12,14 @@ const hiddenButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 
-const lettersNotInWord = [];
+const guessedLetters = [];
 
 const hideLetter = () => {
     // console.log(word);
     for (let letter of word) {
-        wordInProgress.innerText += "●";
+       wordInProgress.innerText += "●";
     }
+
 }
 
 hideLetter(word)
@@ -49,12 +50,40 @@ const checkInput = myLetter => {
 const makeGuess = letter => {
     const letterUpperCase = letter.toUpperCase();
     // check to see if letter already in array using includes
-    if (lettersNotInWord.includes(letterUpperCase)) {
+    if (guessedLetters.includes(letterUpperCase)) {
         console.log("You've already guessed that letter silly!")
         return "You've already guessed that letter silly!"
     } else {
-        lettersNotInWord.push(letterUpperCase);
-        console.log(lettersNotInWord)
+        guessedLetters.push(letterUpperCase);
+        showGuessedLetters(guessedLetters);
+        updateWordInProgress(guessedLetters);
     }
-    return lettersNotInWord
 }
+
+const showGuessedLetters = () => {
+    lettersGuessed.innerHTML = "";
+    // console.log(guessedLetters);
+
+    // iterate over the guessedLetters array to output the li items
+    for (let letter of guessedLetters) {
+        const listLetters = document.createElement("li");
+        listLetters.innerHTML = letter;
+        lettersGuessed.append(listLetters);
+    }
+}
+
+const updateWordInProgress = () => {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    console.log(wordArray)
+    const lettersInWord = [];
+    for (let i = 0; i < wordArray.length; i++) {
+        for (let j = 0; j < guessedLetters.length; j++) {
+            if (wordArray[i] === guessedLetters[j]) {
+                lettersInWord.push(wordArray[i]);
+            }
+        }
+    }
+    return lettersInWord;
+}
+
