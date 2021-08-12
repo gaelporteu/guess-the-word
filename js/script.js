12257bb298642ee3b97d46a085e3a3fea05f58d4
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-08-12 01:39:22 -0400

    Update makeGuess function

--- js/script.js
+++ js/script.js
@@ -16,16 +16,58 @@ const playerMessage = document.querySelector(".message");
 const hiddenPlayAgainButton = document.querySelector(".play-again.hide");
 // Magnolia is your starting word to test out the game
 const word = "Magnolia";
+// array will contain all the letters the player guesses
+const guessedLetters = [];
 
 // circle symbols (●) to represent each letter in the word
 const updateWordInProgress = (word) => {
-    lettersInWord = [];
-    for (const letter of word) {
-        console.log(letter)
-        lettersInWord.push("●")
-    }
-    wordInProgress.innerHTML = lettersInWord.join("")
+  // empty arry to place each letter into
+  lettersInWord = [];
+  for (const letter of word) {
+    console.log(letter)
+    // push each letter into the array
+    lettersInWord.push("●")
+  }
+  // output the string of dots to the html element
+  wordInProgress.innerHTML = lettersInWord.join("")
 }
-updateWordInProgress(word)
+updateWordInProgress(word);
 
+// event listener for when a player clicks the Guess button
+guessButton.addEventListener("click", e => {
+  // prevent the default behavior of clicking a button in a form
+  e.preventDefault();
+  // variable to capture the value of the input
+  const inputValue = playerInput.value;
+  console.log(inputValue);
+  // empty the value of the input
+  playerInput.value = "";
+  // empty the message element
+  playerMessage.innerHTML = "";
+  // call checkIsInputLetter using inputValue as argument
+  checkIsInputLetter(inputValue);
+});
 
+// function that accepts the input value as a parameter
+const checkIsInputLetter = (inputValue) => {
+  // regular expression to ensure the player inputs a letter 
+  const acceptedLetter = /[a-zA-Z]/;
+  
+  // check if the input is empty
+  if (inputValue === "") {
+    console.log("Please enter a letter");
+    return "Please enter a letter";
+  } else if (inputValue.length > 1) {
+    console.log("You can only enter one letter");
+    return "You can only enter one letter";
+  } else if (!inputValue.match(acceptedLetter)) {
+    console.log("You can only enter alpha character");
+    return "You can only enter alpha character";
+  }
+  return inputValue;
+}
+
+const makeGuess = letter => {
+    // const letterUpperCase = letter.toUpperCase();
+    console.log(letter);
+  }
